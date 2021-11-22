52b568f01af59a20d890be9083c7dea86f1a2c64
ititiu20012 <ititiu20012@student.hcmiu.edu.vn>
2021-11-22 15:29:09 +0700

    Update Exercise5.c

--- src/Exercise5.c
+++ src/Exercise5.c
@@ -17,7 +17,18 @@ Ex:
 
 void Ex5(char *str){
 	//Your codes here
-	
+	int i = 0;
+	int flag = 0;
+	while(str[i++] != '\0')
+		{flag++;}
+	for(int i = 0; i <= (flag / 2) - 1; i++){
+		if(!(str[i] + 1 == str[flag - 1 - i] || str[i] + 2 == str[flag - 1 - i]))
+		{
+			printf("Invalid!");
+			return;
+		   }
+		}
+			printf("Valid!");
 }
 
 int main(int argc, char *argv[]) {
